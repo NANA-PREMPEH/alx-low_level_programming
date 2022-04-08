cb6ca7558b012fa7a98e76177254346b8b26978d
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-04-08 07:07:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: alphabet in lowercase, followed by a new line.
+ *
+ * Always return(0)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	puchar('\n');
+
+	return(0);
+
+}
+
+
