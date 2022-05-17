93d32bdb5c7fd5c176d10acd79505a717e80d8dc
NANA-PREMPEH <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-05-17 03:45:40 +0000

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,19 +15,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list appa;
 	unsigned int index_i;
 
-	/* initialize the argument list from the start */
+	
 	va_start(appa, n);
 
-	/* iterate through each argument*/
+	
 	for (index_i = 0; index_i < n; index_i++)
 	{
-		/* print next argument */
+		
 		printf("%d", va_arg(appa, int));
-		/* print separator only between arguments */
+		
 		if (separator && index_i != n - 1)
 			printf("%s", separator);
 	}
-	/*clean up*/
+	
 	va_end(appa);
 	printf("\n");
 }
