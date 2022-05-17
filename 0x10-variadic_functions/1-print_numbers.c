4ae94d44ecfe1b5355419f3cc42dcbffa9022205
NANA-PREMPEH <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-05-17 03:30:28 +0000

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,14 +10,14 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     va_list v_list;
-    unsigned int i;
+    unsigned int index;
 
     va_start(v_list, n);
 
-    for (i = 0; i < n; i++)
+    for (index = 0; index < n; index++)
     {
         printf("%d", va_arg(v_list, int));
-        if (separator && i < n - 1)
+        if (separator && index < n - 1)
             printf("%s", separator);
     }
 
