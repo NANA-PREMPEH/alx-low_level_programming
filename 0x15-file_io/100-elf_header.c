43271e6a1b6aec38b7f4e618fd8a9eace8973984
NANA-PREMPEH <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-05-31 03:20:35 +0000

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -273,10 +273,10 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int openfile, readfile;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	openfile = open(argv[1], O_RDONLY);
+	if (openfile == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -284,15 +284,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(openfile);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	readfile = read(openfile, header, sizeof(Elf64_Ehdr));
+	if (readfile == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(openfile);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -309,6 +309,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(openfile);
 	return (0);
 }
